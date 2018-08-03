4a565e043e677280c0b5ac942e97bd250810b3c9
Daniel <dbalastegui@gmail.com>
2018-08-03 22:00:25 +0200

    initial architecture and navbar

---
+++ src/components/views/Navbar.vue
@@ -0,0 +1,115 @@
+<template lang="pug">
+nav.navbar.navbar-expand-md
+  .navbar-collapse.collapse.w-100.order-1.order-md-0.dual-collapse2
+    ul.navbar-nav.mr-auto
+  .mx-auto.order-0
+    router-link.navbar-brand.mx-auto(to="/") KUDOWALL
+    button.navbar-toggler(type='button')
+      span.navbar-toggler-icon
+  .navbar-collapse.collapse.w-100.order-3.dual-collapse2
+    ul.navbar-nav.ml-auto
+      li.nav-item.mx-2.my-auto
+        router-link.nav-link(to="/new")
+          button.btn.btn-sm.btn-outline-primary(type='button')
+            add-circle-outline-icon(title="Add new kudos", w="25px", h="25px")
+            span {{ $t('New Kudo') }}
+      li.nav-item.mx-2.my-auto
+        a.nav-link(href='#', @click="toggleFullscreen")
+          component(:is="fullscreenIcon", :title="$t('Add new Kudo')", w="25px", h="25px")
+</template>
+
+<script>
+import ExpandIcon from 'vue-ionicons/dist/md-expand.vue';
+import ContractIcon from 'vue-ionicons/dist/md-contract.vue';
+import AddCircleOutlineIcon from 'vue-ionicons/dist/md-add-circle-outline.vue';
+
+/* Get the documentElement (<html>) to display the page in fullscreen */
+const elem = document.documentElement;
+
+/* View in fullscreen */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) { /* Firefox */
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE/Edge */
+    document.msExitFullscreen();
+  }
+}
+
+function isFullscreen() {
+  return window.screenTop || window.screenY;
+}
+
+export default {
+  name: 'navbar',
+  data() {
+    return {
+      isFullscreen: false,
+    };
+  },
+  created() {
+    document.addEventListener('webkitfullscreenchange', () => {
+      this.isFullscreen = isFullscreen();
+    }, false);
+
+    document.addEventListener('mozfullscreenchange', () => {
+      this.isFullscreen = isFullscreen();
+    }, false);
+
+    document.addEventListener('fullscreenchange', () => {
+      this.isFullscreen = isFullscreen();
+    }, false);
+  },
+  methods: {
+    toggleFullscreen() {
+      if (this.isFullscreen) closeFullscreen();
+      else openFullscreen();
+    },
+  },
+  computed: {
+    fullscreenIcon() {
+      return (this.isFullscreen) ? 'contract-icon' : 'expand-icon';
+    },
+  },
+  components: {
+    ExpandIcon,
+    ContractIcon,
+    AddCircleOutlineIcon,
+  },
+};
+</script>
+
+<style lang="scss">
+@import '../../assets/bootstrap';
+
+.navbar-brand {
+  font-weight: 500;
+  font-size: 1.2rem;
+  letter-spacing: 0.5rem;
+}
+
+.navbar {
+  background-color: $gray-100;
+}
+
+.nav-link {
+  padding: 0 1rem;
+}
+</style>
+
